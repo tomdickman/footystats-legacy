f50ae7d054d1d46bad2f55376322dbd28cbcf692
Tom Dickman <twdickman@gmail.com>
2022-06-09 12:02:18 +1000

    feat: add ECS task and service to infrastructure

--- infra/main.tf
+++ infra/main.tf
@@ -28,3 +28,78 @@ resource "aws_ecr_repository" "footystats_web_ecr_repo" {
 resource "aws_ecs_cluster" "footystats_cluster" {
   name = "footystats_cluster"
 }
+
+resource "aws_ecs_task_definition" "footystats_web_task" {
+  family                   = "footystats_web_task"
+  container_definitions    = jsonencode([
+    {
+      "name": "footystats_web_task",
+      "image": "${aws_ecr_repository.footystats_web_ecr_repo.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ])
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
+
+resource "aws_ecs_service" "footystats_web_service" {
+  name            = "footystats_web_service"                             # Naming our service
+  cluster         = "${aws_ecs_cluster.footystats_cluster.id}"           # Referencing our created Cluster
+  task_definition = "${aws_ecs_task_definition.footystats_web_task.arn}" # Referencing the task our service will spin up
+  launch_type     = "FARGATE"
+  desired_count   = 1 # Setting the number of containers we want deployed
+
+  network_configuration {
+    subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
+    assign_public_ip = true # Providing our containers with public IPs
+  }
+}
+
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Providing a reference to our default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "ap-southeast-2a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "ap-southeast-2b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "ap-southeast-2c"
+}
