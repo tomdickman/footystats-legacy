20ae7497e75f26eb82780fa3ac3514144eaa8257
Tom Dickman <twdickman@gmail.com>
2022-06-09 13:28:15 +1000

    feat: Add ALB and forwarding rules to expose web app

--- infra/main.tf
+++ infra/main.tf
@@ -60,9 +60,32 @@ resource "aws_ecs_service" "footystats_web_service" {
   launch_type     = "FARGATE"
   desired_count   = 1 # Setting the number of containers we want deployed
 
+  load_balancer {
+    target_group_arn = "${aws_lb_target_group.footystats_web_target_group.arn}" # Referencing our target group
+    container_name   = "${aws_ecs_task_definition.footystats_web_task.family}"
+    container_port   = 80 # Specifying the container port
+  }
+
   network_configuration {
     subnets          = ["${aws_default_subnet.default_subnet_a.id}", "${aws_default_subnet.default_subnet_b.id}", "${aws_default_subnet.default_subnet_c.id}"]
     assign_public_ip = true # Providing our containers with public IPs
+  }
+}
+
+resource "aws_security_group" "footystats_web_service_security_group" {
+  ingress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    # Only allowing traffic in from the load balancer security group
+    security_groups = ["${aws_security_group.footystats_web_alb_security_group.id}"]
+  }
+
+  egress {
+    from_port   = 0 # Allowing any incoming port
+    to_port     = 0 # Allowing any outgoing port
+    protocol    = "-1" # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
   }
 }
 
@@ -102,4 +125,56 @@ resource "aws_default_subnet" "default_subnet_b" {
 
 resource "aws_default_subnet" "default_subnet_c" {
   availability_zone = "ap-southeast-2c"
+}
+
+resource "aws_alb" "footystats_web_alb" {
+  name               = "footystats-web-alb"
+  load_balancer_type = "application"
+  subnets = [
+    "${aws_default_subnet.default_subnet_a.id}",
+    "${aws_default_subnet.default_subnet_b.id}",
+    "${aws_default_subnet.default_subnet_c.id}"
+  ]
+  # Referencing the security group
+  security_groups = ["${aws_security_group.footystats_web_alb_security_group.id}"]
+}
+
+# Creating a security group for the load balancer:
+resource "aws_security_group" "footystats_web_alb_security_group" {
+  ingress {
+    from_port   = 80 # Allowing traffic in from port 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+  }
+
+  egress {
+    from_port   = 0 # Allowing any incoming port
+    to_port     = 0 # Allowing any outgoing port
+    protocol    = "-1" # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
+
+resource "aws_lb_target_group" "footystats_web_target_group" {
+  name        = "footystats-web-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = "${aws_default_vpc.default_vpc.id}" # Referencing the default VPC
+  health_check {
+    matcher = "200,301,302"
+    path = "/"
+  }
+}
+
+# Register a forwarding rule for HTTP from ALB to ECS through target group.
+resource "aws_lb_listener" "footystats_web_http_listener" {
+  load_balancer_arn = "${aws_alb.footystats_web_alb.arn}" # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.footystats_web_target_group.arn}" # Referencing our target group
+  }
 }
