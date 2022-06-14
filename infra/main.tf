40631f0f5cef7bea191b1862142c28cb6f891e28
Tom Dickman <twdickman@gmail.com>
2022-06-14 14:26:51 +1000

    refactor: amend to use cert for www address

--- infra/main.tf
+++ infra/main.tf
@@ -200,15 +200,15 @@ resource "aws_lb_listener" "footystats_web_http_listener" {
   port              = "80"
   protocol          = "HTTP"
   default_action {
-    type             = "redirect"
+    type = "redirect"
     redirect {
-      host = "#{host}"
-      path = "/#{path}"
-      query = "#{query}"
+      host        = "#{host}"
+      path        = "/#{path}"
+      query       = "#{query}"
       port        = "443"
       protocol    = "HTTPS"
       status_code = "HTTP_301"
-    }  
+    }
   }
 }
 
@@ -219,26 +219,37 @@ data "aws_route53_zone" "aflfootystats_hosted_zone" {
 }
 
 resource "aws_acm_certificate" "footystats_web_cert" {
-  domain_name       = "*.aflfootystats.com"
-  validation_method = "DNS"
+  domain_name               = "www.aflfootystats.com"
+  validation_method         = "DNS"
+  subject_alternative_names = ["aflfootystats.com"]
   lifecycle {
     create_before_destroy = true
   }
 }
 
-# Add a record pointing to our cert for TLS
+# Add a records pointing to our cert for TLS
 resource "aws_route53_record" "footystats_web_cert_record" {
-  zone_id = data.aws_route53_zone.aflfootystats_hosted_zone.zone_id
-  name    = one(aws_acm_certificate.footystats_web_cert.domain_validation_options).resource_record_name
-  records = [one(aws_acm_certificate.footystats_web_cert.domain_validation_options).resource_record_value]
-  type    = one(aws_acm_certificate.footystats_web_cert.domain_validation_options).resource_record_type
+  zone_id         = data.aws_route53_zone.aflfootystats_hosted_zone.zone_id
+  allow_overwrite = true
+
+  for_each = {
+    for dvo in aws_acm_certificate.footystats_web_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  name    = each.value.name
+  records = [each.value.record]
   ttl     = 60
+  type    = each.value.type
 }
 
 # This tells terraform to cause the route53 validation to happen
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.footystats_web_cert.arn
-  validation_record_fqdns = [aws_route53_record.footystats_web_cert_record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.footystats_web_cert_record : record.fqdn]
 }
 
 # Redirect all traffic to the ALB
